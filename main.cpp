69bf3bbb85823e4e5c27b8c1f7cc3d40d0bc36b5
PESolut <jahaadpetty@pursuit.org>
2023-08-01 15:26:35 -0400

    set the color to a color of our choice (lapis luzil )

--- main.cpp
+++ main.cpp
@@ -55,9 +55,21 @@ int main() {
         // Process input every iteration of the loop
         processInput(window);
 
+        // Render commands here
+
+        // setting colors to be a float value between 0 and 1 (RPG value / 255)
+        // see:  https://stackoverflow.com/questions/46524896/understanding-glclearcolor-values
+        // also: https://registry.khronos.org/OpenGL-Refpages/gl4/html/glClearColor.xhtml
+        float rValue = 51.0f / 255.0f;
+        float gValue = 102.0f / 255.0f;
+        float bValue = 153.0f / 255.0f;
+        glClearColor(rValue, gValue, bValue, 1.0f); // Set the color of the screen by changing state of OpenGL ( state setting )
+        glClear(GL_COLOR_BUFFER_BIT); // Clear the screen with the color we set in glClearColor ( state using )
+
+
         // Swap buffers and poll events
-        glfwSwapBuffers(window);
         glfwPollEvents();
+        glfwSwapBuffers(window);
         }
     // Terminate GLFW
     glfwTerminate();
