8cfb7b78380b7c606c663839e85227a917c6a46a
PESolut <jahaadpetty@pursuit.org>
2023-08-01 15:08:31 -0400

    window rendered + added input function + adjusting window size on every tick

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 #include <iostream>
 
 // Callback function for window resizing
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
-}  
+}
+
+// Function to process input
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main() { 
     // Initialize GLFW
@@ -16,13 +21,11 @@ int main() {
         return -1;
     }
 
-    // Set GLFW window hints
+    // Set GLFW window hints ( we are using version 3.3, we also are forward compatible)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-
 
     // Create GLFW window
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
@@ -31,15 +34,16 @@ int main() {
         glfwTerminate();
         return -1;
     }
+
     // Make the current window the main context
     glfwMakeContextCurrent(window);
 
     // Load GLAD
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
             std::cout << "Failed to initialize GLAD" << std::endl;
             return -1;
     }   
+
     // Set the viewport
     glViewport(0, 0, 800, 600);
 
@@ -47,11 +51,15 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
     // Render loop
-    while(!glfwWindowShouldClose(window))
-    {
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-    }
+    while(!glfwWindowShouldClose(window)){
+        // Process input every iteration of the loop
+        processInput(window);
 
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        }
+    // Terminate GLFW
+    glfwTerminate();
     return 0;
 }
