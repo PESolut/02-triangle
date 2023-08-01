bf50437e1a34a82d60fae0bb29a5ccd4dc2d0dc2
PESolut <jahaadpetty@pursuit.org>
2023-08-01 14:35:49 -0400

    bug fixes from project not having imported libs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 
-int main() {
-    // Your OpenGL code here
+int main() { 
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    // glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
     return 0;
 }
